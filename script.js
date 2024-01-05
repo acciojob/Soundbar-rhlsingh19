e1bd572a1126e7b326a321a45126338b9feb6a2a
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-05 12:00:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function playSound(soundFileName) {
+  const audio = new Audio(`sounds/${soundFileName}`);
+  audio.play();
+}
+
+function stopAllSounds() {
+  const allAudio = document.querySelectorAll('audio');
+  allAudio.forEach(audio => audio.pause());
+}
